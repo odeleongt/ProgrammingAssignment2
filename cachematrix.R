cb7a7e5fef6e39142f06a77c8c7b2d8658250aef
odeleongt [nb] <odeleongt@gmail.com>
2015-01-09 10:09:50 -0600

    Document the assignment with comments in the R file.

--- cachematrix.R
+++ cachematrix.R
@@ -1,24 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+##
+## Matrix object with cacheable solve
+## By odeleongt, based on the makeVector and cachemean examples provided in
+## the instructions of the Programming Assignment 2, rprog-007 class in Coursera
+##
+## The functions can create a cacheable matrix object which can "remember" if it
+## already knows its inverse and avoid calculating it again. The object can only
+## be used (directly) with the function cacheMatrix.
 
-## Write a short comment describing this function
+
+## The function makeCacheMatrix creates a special object which stores a matrix
+## and provides methods to get or set the matrix value, and to set or get the
+## inverse of the matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
+  
+  # Function to set the value of the object (e.g. cached$set(matrix()))
   set <- function(y) {
     x <<- y
     m <<- NULL
   }
+  
+  # Function to get the value of the object (e.g. cached$get())
   get <- function() x
+  
+  # Function to set the inverse of the object (e.g. cached$setsolve(solve(matrix())))
   setsolve <- function(solve) m <<- solve
+  
+  # Function to get the inverse of the object (e.g. cached$getsolve())
   getsolve <- function() m
+  
+  # Return the special object, which consists of a list of the methods
   list(set = set, get = get,
        setsolve = setsolve,
        getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## This function calculates or gets the inverse of a matrix created with
+## makeCacheMatrix. If the object already knows its inverse, the function gets
+## it and avoids performing the calculation. Otherwise, it uses solve to get the
+## inverse. For this function to work the matrix needs to be square 
+## (e.g. `solve(1:6, ncol=2)` does not work) and not exactly singular 
+## (e.g. `solve(matrix(1:16, ncol=4))` does not work). See the included
+## reproducible examples below.
 
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
@@ -27,6 +52,9 @@ cacheSolve <- function(x, ...) {
     message("getting cached data")
     return(m)
   }
+  
+  # If there was no cached results, calculate them, using the objects methods
+  # to get the data and set the inverse.
   data <- x$get()
   m <- solve(data, ...)
   x$setsolve(m)
@@ -35,7 +63,10 @@ cacheSolve <- function(x, ...) {
 
 
 
-## Include some minimum reproducible examples
+## Include some minimum reproducible examples, you can use these examples (after
+## runing the code which defines the makeCacheMatrix and cacheSolve functions)
+## to thest that the functions work (and that there is indeed a speedup when
+## using the cached inverse).
 
 
 # Test matrix
